bea1073eab8fcbc31572774c6c6b2145d1f36d18
Abdullah <abdullah.4110r.work@gmail.com>
2024-11-13 18:42:31 +0500

    feat:test passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    let str = '';
+    for (let i = 0;i < num;i++) {
+        str = str + string;
+    }
+    return str;
 };
-
+repeatString('hey',3);
 // Do not edit below this line
 module.exports = repeatString;
