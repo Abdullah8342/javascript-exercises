3897908a0b162f2a9e38d2479aebc2dccdf6b71a
Abdullah <abdullah.4110r.work@gmail.com>
2024-11-13 19:37:15 +0500

    feat:Test passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reverse = '';
+    for (let i = string.length - 1;i >= 0;i--) {
+        reverse = reverse + string[i];
+    }
+    return reverse;
 };
-
+reverseString('hello there')
 // Do not edit below this line
 module.exports = reverseString;
