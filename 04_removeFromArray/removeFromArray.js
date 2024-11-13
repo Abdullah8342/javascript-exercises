a21a5799e248c095ea89515d8c33b26f8b5c4223
Abdullah <abdullah.4110r.work@gmail.com>
2024-11-13 20:31:21 +0500

    feat:Test padded

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(Array,item) {
+    let NewArray = [];
+    for (let i = 0;i < Array.length;i++) {
+        if (Array[i] === item) {
+            continue;
+        }
+        else {
+            NewArray.push(Array[i]);
+        }
+    }
+    return NewArray;
 };
-
+console.log(removeFromArray([1, 2, 3, 4], 3));
 // Do not edit below this line
 module.exports = removeFromArray;
