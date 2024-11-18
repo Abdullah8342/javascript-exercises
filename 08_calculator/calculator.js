430a85e6f379eb773c1746aa42d218b8401d14f0
Abdullah <abdullah.4110r.work@gmail.com>
2024-11-18 10:42:03 +0500

    feat:Adding,Subtraction,Sum,Multipication,Power,Factorial Making multiple function for performing task using loop

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,42 @@
-const add = function() {
-	
+const add = function(firstNum,secondNum) {
+  return firstNum + secondNum;
 };
 
-const subtract = function() {
-	
+const subtract = function(firstNum,secondNum) {
+	return firstNum - secondNum;
 };
 
-const sum = function() {
-	
+const sum = function(Array) {
+  let sumOfAll = 0;
+	for (let A = Array.length - 1;A >= 0;A--) {
+    sumOfAll += Array[A];
+  }
+  return sumOfAll;
 };
 
-const multiply = function() {
-
+const multiply = function(firstNum,secondNum) {
+  return (firstNum * secondNum);
 };
 
-const power = function() {
-	
+const power = function(base,exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let factorialOfNumber = 0;
+	for (let i = number;i > 0;i--) {
+    factorialOfNumber += number * (number - i);
+  }
+  return factorialOfNumber;
 };
+
+console.log(add(2,2));
+console.log(subtract(5,5));
+console.log(sum([1,2,3,4,5]));
+console.log(multiply(2 , 2));
+console.log(power(2,2));
+console.log(factorial(4));
+
 
 // Do not edit below this line
 module.exports = {
