57a739152a3ba17d6a0017fd5460fd4656149864
Abdullah <abdullah.4110r.work@gmail.com>
2024-11-18 14:55:17 +0500

    feat:Using loop to reverse the word using if else condition to the revser word is palindrome or not

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let wordLength = word.length - 1;
+    let reverseTheWord = '';
+    while (wordLength >= 0) {
+        reverseTheWord += word[wordLength];
+        wordLength--;
+    }
+    if (word === reverseTheWord) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
+console.log(palindromes('tacos'));
+console.log(palindromes('racecar'))
 // Do not edit below this line
 module.exports = palindromes;
