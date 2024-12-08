58f1ae19254fe4f38a3802bcec0e1ca23c9c26ef
Abdullah <abdullah.4110r.work@gmail.com>
2024-12-08 21:45:07 +0500

    feat:Fibonacci Adding functionality of fibonacci in this program

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+   let Array = [1,1]
+   for (let x = 0;x <= number;x++){
+    A = Array[x] + Array[x + 1];
+    Array.push(A);
+   }
+   // console.log(Array.length);
+   return Array[number - 1];
 };
-
+// fibonacci(6)
+console.log(fibonacci(4));
+console.log(fibonacci(6));
+console.log(fibonacci(10));
 // Do not edit below this line
 module.exports = fibonacci;
