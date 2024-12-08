f8a47a077c59f9b807e65088fde556046bf8f5dc
Abdullah <abdullah.4110r.work@gmail.com>
2024-12-08 22:31:06 +0500

    feat:Getting data from Array and making a New Arry Making a function that take An array and get data from it and making a new Array from this Array and return it to the Array

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,24 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(Array) {
+    let lengthOfArray = Array.length
+    let Ary = [];
+    for (let x = 0; x < lengthOfArray;x++){
+        let A = Array[x].title;
+        Ary.push(A);
+    }
+    return Ary;
 };
 
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+]
+
+console.log(getTheTitles(books));
 // Do not edit below this line
 module.exports = getTheTitles;
