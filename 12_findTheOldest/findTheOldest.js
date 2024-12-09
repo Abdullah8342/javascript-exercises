c56e796d7f88d8515a559d5d35eca66f2a299e0f
Abdullah <abdullah.4110r.work@gmail.com>
2024-12-09 13:48:15 +0500

    feat:Adding functionality Taking Array that containing object of peoples and returning the oldest one from the list

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,29 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(Array) {
+    let lenghtOfArray = Array.length;
+    let Age = [];
+    for (let x = 0;x < lenghtOfArray;x++) {
+        let A = Array[x].deathYear - Array[x].birthYear
+        Age.push(A);
+    }
+    let oldest = Age[0];
+    let index = 0;
+    for (let x = 0;x < lenghtOfArray;x++) {
+        if (Age[x] > oldest) {
+            oldest = Age[x];
+            index = x;
+        }
+    }
+    return (Array[index].name);
 };
+
+
+const people = [
+    { name: "Alice", birthYear: 1940, deathYear: 2020 },
+    { name: "Bob", birthYear: 1920, deathYear: 2010 },
+    { name: "Charlie", birthYear: 1930, deathYear: 2021 }
+];
+
+console.log(findTheOldest(people)); // Output: "Alice"
 
 // Do not edit below this line
 module.exports = findTheOldest;
